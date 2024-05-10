677941fdf16f44de282bd30d9d1c503f2c7f460b
Bhaskar N Prasad <119781809+Bhaskar-N-Prasad@users.noreply.github.com>
2024-05-10 18:04:44 +0000

    about

--- src/components/AboutUs/Aboutus.jsx
+++ src/components/AboutUs/Aboutus.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Aboutus() {
   return (
-    <div className='flex justify-center text-2xl bg-[#f33232]'>
+    <div className='flex justify-center text-4xl font-bold bg-[#ffff41] mx-auto max-w-2xl py-32 sm:py-48 lg:py-5'>
       About us 
     </div>
   )
