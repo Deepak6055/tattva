0ce28c7737bad4cf089a4170f5e0249ddc68b96c
Bhaskar N Prasad <119781809+Bhaskar-N-Prasad@users.noreply.github.com>
2024-05-10 18:20:34 +0000

    haha

--- src/components/AboutUs/Aboutus.jsx
+++ src/components/AboutUs/Aboutus.jsx
@@ -7,11 +7,12 @@ function Aboutus() {
                   About Us
             </h1>
             <div>
-            <p className="mt-12 text-lg leading-8 text-gray-600">
+            <p className="text p-2 text-justify mt-12 text-lg leading-8 text-gray-600">
             The Tattva Project is a pioneering initiative with a mission to empower students by creating a programming education system that transcends language barriers. We focus on building a user-friendly environment based on Python, specifically designed for learners who speak and write in Kannada. Our aim is to democratize coding education, ensuring that language is not a barrier to acquiring essential programming skills.
       </p>
-      <br />
-      
+      <p className="mt-12 text-justify text-lg leading-8 text-gray-600">
+      At the heart of the Tattva Project is an innovative system that allows students to write and execute Python code in Kannada, using English alphabets for transliteration. This approach makes programming more accessible, inviting a broader range of students to explore the world of coding without the additional challenge of mastering English.
+      </p>
     </div>
     </div>
   );
