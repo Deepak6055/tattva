d85aebd51220167b5a4bd821d848cbb6a8224ae1
Deepak6055 <127471071+Deepak6055@users.noreply.github.com>
2024-05-10 19:02:45 +0000

    vairablesmadene

--- src/components/Documentation/documents/Variables.jsx
+++ src/components/Documentation/documents/Variables.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Card, CardContent, Button, CardBody } from '@material-tailwind/react';
+import { Card, CardBody, Button } from '@material-tailwind/react';
 
 function Variables() {
-  const [isCopied, setIsCopied] = useState(false); // Define isCopied state variable
+  const [isCopiedLeft, setIsCopiedLeft] = useState(false); // Define state variable for left block
+  const [isCopiedRight, setIsCopiedRight] = useState(false); // Define state variable for right block
 
   const code = `def swap_numbers(a, b):
   # Swapping logic using a temporary variable
@@ -23,85 +24,127 @@ print("After swapping:")
 print("First number:", num1)
 print("Second number:", num2)`;
 
+
   const output = `Enter first number: 10
 Enter second number: 20
 After swapping:
 First number: 20.0
 Second number: 10.0`;
 
-  const handleCopy = () => {
-    setIsCopied(true);
+  const code2 = `bari("ondu anke bareyiri")
+akshara a = odu()
+bari("innondu anke bareyiri")
+akshara b = odu()
+bari("badalisuva munna")
+bari("a = " + a)
+bari("b = " + b)
+akshara c = a
+akshara a = b
+akshara b = c
+bari("badalisida nantara")
+bari("a = " + a)
+bari("b = " + b)`
+  const output2 = `tattva > odisu("example/badalisu.myopl")
+ondu anke bareyiri
+1
+innondu anke bareyiri
+2
+badalisuva munna
+a = 1
+b = 2
+badalisida nantara
+a = 2
+b = 1`
+
+  const handleCopyLeft = () => {
+    setIsCopiedLeft(true);
     setTimeout(() => {
-      setIsCopied(false);
+      setIsCopiedLeft(false);
+    }, 2000);
+  };
+
+  const handleCopyRight = () => {
+    setIsCopiedRight(true);
+    setTimeout(() => {
+      setIsCopiedRight(false);
     }, 2000);
   };
 
   return (
     <div className="p-6 bg-light-grey min-h-screen">
-      <h1 className="text-4xl font-bold mb-4 text-dark-blue">Variable example</h1>
-      {/* this is both our code and python code division */}
-      <div className='flex flex-row gap-4'>
-        {/* block right */}
-        <div className="w-1/2">
-          <Card className="mt-6 border-solid rounded-md bg-[#e1d0d0] shadow-lg">
-            <CardBody>
-              <h2 className="text-2xl font-bold mb-2 text-dark-blue">Code</h2>
-              <div className="flex items-center bg-[#a89292] rounded p-2">
-                <pre className="mb-0 mr-2 language-python overflow-x-auto">
-                  <code className="language-python ">{code}</code>
-                </pre>
-                <CopyToClipboard text={code} onCopy={handleCopy}>
-                  <Button ripple="light" className="ml-2 bg-[#726262]">
-                    {isCopied ? 'Copied!' : 'Copy'}
-                  </Button>
-                </CopyToClipboard>
-              </div>
-            </CardBody>
-          </Card>
-
-          <Card className='bg-[#e1d0d0]'>
-            <CardBody>
-              <h2 className="text-2xl font-bold mb-2 text-dark-blue ">Output</h2>
-              <div className="flex items-center bg-light-grey rounded p-2">
-                <pre className="mb-0 mr-2 language-python">
-                  <code className="language-python">{output}</code>
-                </pre>
-              </div>
-            </CardBody>
-          </Card>
+      <h1 className="text-4xl font-bold mb-4 text-dark-blue">Variable</h1>
+      <div>
+        {/* explination of what is a varaiable */}
+        <div>
+          <div className="text-dark-blue bg-[#e1d0d0] p-4 rounded-md mb-4">
+            Variables are fundamental elements in programming used to store and manipulate data. Think of them as containers that hold values such as numbers, text, or more complex data structures like arrays or objects. In programming, variables serve several crucial purposes.
+          </div>
         </div>
-        {/* block left */}
-        <div className='w-1/2'>
-          <Card className="mt-6 w w-fit border-solid rounded-md bg-[#e1d0d0] shadow-lg">
-            <CardBody>
-              <h2 className="text-2xl font-bold mb-2 text-dark-blue">Code</h2>
-              <div className="flex items-center bg-[#a89292] rounded p-2">
-                <pre className="mb-0 mr-2 language-python overflow-x-auto">
-                  <code className="language-python ">{code}</code>
-                </pre>
-                <CopyToClipboard text={code} onCopy={handleCopy}>
-                  <Button ripple="light" className="ml-2 bg-[#726262]">
-                    {isCopied ? 'Copied!' : 'Copy'}
-                  </Button>
-                </CopyToClipboard>
-              </div>
-            </CardBody>
-          </Card>
 
-          <Card className='bg-[#e1d0d0]'>
-            <CardBody>
-              <h2 className="text-2xl font-bold mb-2 text-dark-blue ">Output</h2>
-              <div className="flex items-center bg-light-grey rounded p-2">
-                <pre className="mb-0 mr-2 language-python">
-                  <code className="language-python">{output}</code>
-                </pre>
-              </div>
-            </CardBody>
-          </Card>
+        {/* this is code and stuff  */}
+        <div className='flex flex-row gap-4'>
+          <div className="w-1/2">
+            <Card className="mt-6 border-solid rounded-md bg-[#e1d0d0] shadow-lg">
+              <CardBody>
+                <h2 className="text-2xl font-bold mb-2 text-dark-blue">Code</h2>
+                <div className="flex items-center bg-[#a89292] rounded p-2">
+                  <pre className="mb-0 mr-2 language-python overflow-x-auto">
+                    <code className="language-python ">{code}</code>
+                  </pre>
+                  <CopyToClipboard text={code} onCopy={handleCopyLeft}>
+                    <Button className="ml-2 bg-[#726262]">
+                      {isCopiedLeft ? 'Copied!' : 'Copy'}
+                    </Button>
+                  </CopyToClipboard>
+                </div>
+              </CardBody>
+            </Card>
+
+            <Card className='mt-4 bg-[#e1d0d0] rounded-md'>
+              <CardBody>
+                <h2 className="text-2xl font-bold mb-2 text-dark-blue">Output</h2>
+                <div className="flex items-center bg-light-grey rounded p-2">
+                  <pre className="mb-0 mr-2 language-python">
+                    <code className="language-python">{output}</code>
+                  </pre>
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+
+          <div className='w-1/2'>
+            <Card className="mt-6 border-solid rounded-md bg-[#e1d0d0] shadow-lg">
+              <CardBody>
+                <h2 className="text-2xl font-bold mb-2 text-dark-blue">Code</h2>
+                <div className="flex items-center bg-[#a89292] rounded p-2">
+                  <pre className="mb-0 mr-2 language-python overflow-x-auto">
+                    <code className="language-python ">{code2}</code>
+                  </pre>
+                  <CopyToClipboard text={code2} onCopy={handleCopyRight}>
+                    <Button className="ml-2 bg-[#726262]">
+                      {isCopiedRight ? 'Copied!' : 'Copy'}
+                    </Button>
+                  </CopyToClipboard>
+                </div>
+              </CardBody>
+            </Card>
+
+            <Card className='mt-4 bg-[#e1d0d0] rounded-md'>
+              <CardBody>
+                <h2 className="text-2xl font-bold mb-2 text-dark-blue">Output</h2>
+                <div className="flex items-center bg-light-grey rounded p-2">
+                  <pre className="mb-0 mr-2 language-python">
+                    <code className="language-python">{output2}</code>
+                  </pre>
+                </div>
+              </CardBody>
+            </Card>
+          </div>
         </div>
       </div>
-      {/* ends here ++++++++++++++++++++++++++++++++++++ */}
+
     </div>
+
   );
 }
 
